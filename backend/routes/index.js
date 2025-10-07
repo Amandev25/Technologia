146d55b0db211f6db62c2013b9ddfc8adead5182
prashantdubeypng <vinoddubey6059@gmail.com>
2025-10-07 19:11:46 +0530

    Add backend post request

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const password = "hello_world";
 const User = require('../model/User');
+const python_url_correect_essay = "http://localhost:8000/correct-essay";
 // used for the genrating the user id and making the schema for the user in the mongodb
-router.use('/',async(req,res)=>{
+router.post('/',async(req,res)=>{
     try{
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   for (let i = 0; i < 10; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  new User({username:result});
+  const data = new User({username:result});
+  await data.save();
   const token = jwt.sign(result);
-  return res.json({message:"login done " ,data:token});
+  return res.json({message:"login done " ,data:token},password);
     }catch(error){
         console.log("server error")
     }
 })
-router.use('/ai/evaluation',async(req,res)=>{
-  
-
+router.post('/ai/evaluation',async(req,res)=>{
+  try{
+    const body = req.body;
+    if(!body.token){
+      return res.json({message:"token is missing"})
+    }
+    const essay = body.essay;
+    const send_data = await axios(python_url_correect_essay,{
+      method: 'POST',
+      body:{
+        essay:essay,
+        token:body.token
+      }
+    })
+    const userid = jwt.decode(body.token,password);
+    const data = User.updatedAt({username:userid},{
+      reports:reports.push(send_data.context)
+    })
+    return res.json({data:send_data})
+  }catch(e){
+console.log("error in the evalution");
+return res.json({message:"error in evalution of the error"})
+  }
 })
